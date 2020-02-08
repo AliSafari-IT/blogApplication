a60735be454ca526384499cd6cf6287c7da5fe0e
AliSafari-IT <r0803103@thomasmore.be>
2020-02-08 15:36:31 +0100

    userProfile.php: to be merged with master!

--- userProfileChange.php
+++ userProfileChange.php
@@ -1,1 +1,11 @@
 <?php
+session_start();
+include "include/functions.php";
+
+$userProfile = getUserProfile($_GET['username']);
+if (!$userProfile) {
+    header("location: login.php");
+    die();
+}
+
+echo $_GET['username'];
